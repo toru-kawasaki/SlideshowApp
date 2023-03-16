49548d2865c3cabcfc07a156de1063dbf66da992
toru-kawasaki <toru_kawasaki@jrass.jp>
2023-03-16 13:21:00 +0900

    再生中に２画面目を表示した時にクロックが止まらない不具合に対応 ・２画面目表示時に再生中であればクロックを削除（停止） ・同じく、進む・戻るボタンを活性化、停止ボタンは再生ボタンにラベル変更

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -78,11 +78,19 @@ class ViewController: UIViewController {
         performSegue(withIdentifier: "toBigImageView", sender: nil)
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // segueから遷移先のResultViewControllerを取得する
+        // segueから遷移先のbigImageViewControllerを取得する
         let bigImageViewController:BigImageViewController = segue.destination as! BigImageViewController
-        // 遷移先のResultViewControllerで宣言しているx, yに値を代入して渡す
+        // 遷移先のbigImageViewControllerで宣言しているimageに値を代入して渡す
         //bigImageViewController.image = "いか01.png"
         bigImageViewController.image = imageArray[index]
+        //タイマー起動中なら停止する。ボタンも活性化する。
+        if self.timer != nil {
+            self.timer.invalidate()
+            self.timer = nil
+            playorstopButton.setTitle("再生", for: .normal)
+            progressButton.isEnabled = true
+            backButton.isEnabled = true
+        }
     }
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
         }
